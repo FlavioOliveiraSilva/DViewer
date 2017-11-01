afbb3d5068f5ef089ef65a237868b9f9ef3946d1
flavio <flavolsilva@gmail.com>
2017-11-01 10:23:12 +0100

    model defined and extra templates

---
+++ fileViewer/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+#from uploads.core.models import Document
+from .models import Document
+
+
+class DocumentForm(forms.ModelForm):
+    class Meta:
+        model = Document
+        fields = (
+            'documentName',
+            'idNumber',
+            'brand',
+            'docType',
+            'normRef',
+            'country',
+            'language',
+            'document'
+                  )
+
